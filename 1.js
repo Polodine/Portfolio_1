ce04a8cf0bf1155819e9aa5f470a81f1d2d8c98a
Polodine <bamzurov72@gmail.com>
2016-04-27 19:52:20 +0300

    Stylized button for press

--- 1.js
+++ 1.js
@@ -1,11 +1,33 @@
 ('use strict')
 $(function(){
+	var coordsButtonForPress = document.getElementById('load_post').getBoundingClientRect();
+	var valForShowButtonForPress = false;
+	if ((coordsButtonForPress.top > 0) && (coordsButtonForPress.bottom > 0)){
+		$('#load_post').popover('show');
+		valForShowButtonForPress = true;
+	}
+	$(window).on('scroll', function(){
+		coordsButtonForPress = document.getElementById('load_post').getBoundingClientRect();
+		if ((coordsButtonForPress.top > 60) && (coordsButtonForPress.top < $(window).height() - 60)){
+			if (!valForShowButtonForPress) {
+				$('#load_post').popover('show');
+				valForShowButtonForPress = true;
+			}
+		}
+		else {
+			if (valForShowButtonForPress) {
+				$('#load_post').popover('hide');
+				valForShowButtonForPress = false;
+			}
+		}
+	})
 	// leaveAComment();
 	var val_for_not_reajaxing = false;
 	$('#load_post, .section_main .post_link > a').on('click', function(e){
 		e.preventDefault();
 		if (val_for_not_reajaxing) return;
 		$('.section_1, .response, .section_2, #load_post').animate({opacity: 0}, 600);
+		$('#load_post').popover('hide');
 		setTimeout(leaveAComment, 650);
 		val_for_not_reajaxing = true;
 	})
@@ -21,7 +43,7 @@ function leaveAComment(){
 	otherPost.classList.add('load_article');
 	document.getElementsByTagName('article')[0].appendChild(otherPost);
 	$('.load_section').load('mainAjax.html', function(response, status, xhr){
-		$('.section_1, .response, .section_2').hide();
+		$('.section_1, .response, .section_2, #load_post').hide();
 		if (status == "error"){
 			$('.load_section').html('<img src="imgs/photo_1/photo1_med.jpg"srcset="imgs/photo_1/photo1_low.jpg 400w,imgs/photo_1/photo1_med.jpg 700w,imgs/photo_1/photo1_high.jpg 1000w,imgs/photo_1/photo1_very_high.jpg 1500w"sizes="100vw"alt="img-style-1" width=100% class="jpeg_img"><img src="imgs/photo_2/photo2_med.jpg"srcset="imgs/photo_2/photo2_low.jpg 400w,imgs/photo_2/photo2_med.jpg 700w,imgs/photo_2/photo2_high.jpg 1000w,imgs/photo_2/photo2_very_high.jpg 1500w"sizes="100vw"alt="img-style-1" width=100%class="jpeg_img"><img src="imgs/photo_3/photo3_med.jpg"srcset="imgs/photo_3/photo3_low.jpg 400w,imgs/photo_3/photo3_med.jpg 700w,imgs/photo_3/photo3_high.jpg 1000w,imgs/photo_3/photo3_very_high.jpg 1500w"sizes="100vw"alt="img-style-1" width=100%class="jpeg_img"><blockquote class="quote_main">"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."</blockquote><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <b>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </b>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p><br><br><div class="btn-group pull-right btn_group_link"><span class="btn btn-group-text chapter_link chapter_link_btn" >SHARE</span><a class="btn btn-default" href="#"><img src="imgs/icons_social/icons_0005_fb.png" alt="facebook_link"></a><a class="btn btn-default" href="#"><img src="imgs/icons_social/icons_0010_tw.png" alt="twitter_link"></a><a class="btn btn-default" href="#"><img src="imgs/icons_social/icons_0009_g+.png" alt="google_link"></a><a class="btn btn-default" href="#"><img src="imgs/icons_social/icons_0008_tumblr.png" alt="tumblr_link"></a><a class="btn btn-default" href="#"><img src="imgs/icons_social/icons_0006_pinterest.png" alt="pinterest_link"></a></div>');
 		}
